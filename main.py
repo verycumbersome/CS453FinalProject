e646a3ca29c677c3ce94161b37977392764f1428
matthew jordan <matthewsages@gmail.com>
2020-12-03 14:46:50 -0800

    Add basefiles

---
+++ main.py
@@ -0,0 +1,152 @@
+import os
+import numpy as np
+import glfw
+import OpenGL
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+from PIL import Image
+
+# Import opengl shapes from file
+import shapes
+
+WINDOW_W = 1500
+WINDOW_H = 1500
+
+display_mode = 4
+
+def lighting():
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_BLEND)
+    glLightfv(GL_LIGHT0, GL_POSITION, [10, 4, 10, 1])
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, [0.8, 1, 0.8, 1])
+    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
+    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
+    glEnable(GL_LIGHT0)
+    return
+
+
+def read_texture(filename):
+    _, file_extension = os.path.splitext(filename)
+    img = Image.open(filename)
+    img_data = np.array(list(img.getdata()), np.int8)
+    texture_id = glGenTextures(1)
+
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+
+    if (file_extension == ".jpg"):
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0,
+                GL_RGB, GL_UNSIGNED_BYTE, img_data)
+
+    if (file_extension == ".png"):
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.size[0], img.size[1], 0,
+            GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+
+    return texture_id
+
+
+def init():
+    glClearColor(0.2, 0.2, 0.2, 0.0)
+    glShadeModel(GL_FLAT)
+
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_TEXTURE_2D)
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+    read_texture("texture2.jpg")
+
+
+def keyboard(bkey, x, y):
+    global display_mode
+
+    key = bkey.decode("utf-8")
+    if key == chr(27):
+        os._exit(0)
+
+    if key == "1":
+        display_mode = 1
+
+    if key == "2":
+        display_mode = 2
+
+    if key == "3":
+        display_mode = 3
+
+    if key == "4":
+        display_mode = 4
+
+    if key == "5":
+        display_mode = 5
+
+    if key == "q":
+        os._exit(0)
+
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glEnable(GL_TEXTURE_2D)
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+
+    # Set Projection window view
+    glViewport(0, 0, WINDOW_H, WINDOW_W)
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(45, (WINDOW_H/WINDOW_W), 0.1, 50.0)
+    glLoadIdentity()
+    glOrtho(-10.0, 10.0, -10.0, 10.0, -100000.0, 100000.0)
+
+    # Set Model view
+    glMatrixMode(GL_MODELVIEW)
+
+    global display_mode
+
+    if display_mode == 1:
+        glRotatef(0.01, 1, 1, 1)
+        shapes.plane(5)
+
+    if display_mode == 2:
+        glRotatef(0.2, 1, 1, 1)
+        shapes.render_shape(4, 4, shapes.cylinder)
+
+    if display_mode == 3:
+        glRotatef(1, 1, 1, 1)
+        shapes.render_shape(10, 10, shapes.cylinder)
+
+    if display_mode == 4:
+        glRotatef(1, 1, 1, 1)
+        shapes.render_shape(10, 10, shapes.sphere, 4)
+
+    if display_mode == 5:
+        glRotatef(1, 1, 1, 1)
+        shapes.render_shape(10, 10, shapes.vase)
+
+    glFlush()
+
+
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(WINDOW_H, WINDOW_W)
+    glutInitWindowPosition(100, 100)
+    wind = glutCreateWindow("Surface Parameterization")
+
+    lighting()
+
+    init()
+
+    glutDisplayFunc(display)
+    glutIdleFunc(display)
+
+    glutKeyboardFunc(keyboard)
+    glutMainLoop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ shapes.py
@@ -0,0 +1,161 @@
+import pdb
+import math
+import glfw
+import numpy as np
+import OpenGL
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+
+def plane(length):
+    glBegin(GL_QUADS)
+    glNormal3f(0.0, 0.0, (length / 2))
+
+    glTexCoord2d(1, 1)
+    glVertex3f((-length / 2), (-length / 2), 0.0)
+    glTexCoord2d(1, 0)
+    glVertex3f((-length  / 2), (length / 2), 0.0)
+    glTexCoord2d(0, 0)
+    glVertex3f((length / 2), (length / 2), 0.0)
+    glTexCoord2d(0, 1)
+    glVertex3f((length / 2), (-length / 2), 0.0)
+
+    glEnd()
+
+
+def cylinder_dydv(u, v):
+    """
+    Function to calculate derivative of cylinder function
+    """
+    radius = 4.0
+    height = 10.0
+    v = math.radians(v * 360.0)
+
+    return(
+        -math.sin(v),
+        math.cos(v),
+        1
+    )
+
+def cylinder_dydu(u, v):
+    """
+    Function to calculate derivative of cylinder function
+    """
+    radius = 4.0
+    height = 10.0
+    v = math.radians(v * 360.0)
+
+    return(
+        0,
+        0,
+        height
+    )
+
+
+def cylinder(u, v):
+    """
+    Function that returns 3d coordinates for a cylinder from 2d input
+    Input: (u, v) -> (height[0-1], theta[0-1])
+    Ouput: (x, y, z) coordinate
+    """
+    radius = 4.0
+    height = 10.0
+    v = math.radians(v * 360.0)
+
+    return(
+        radius * math.cos(v),
+        radius * math.sin(v),
+        u * height
+    )
+
+
+def vase(u, v):
+    """
+    Function that returns 3d coordinates for a cylinder from 2d input
+    Input: (u, v) -> (height[0-1], theta[0-1])
+    Ouput: (x, y, z) coordinate
+    """
+    radius = 4.0
+
+    v = math.radians(v * 360.0)
+    u_rad = math.radians(u * 360.0)
+
+    return(
+        radius * math.cos(u_rad) * math.cos(v),
+        radius * math.cos(u_rad) * math.sin(v),
+        (2 * radius * u) - radius
+    )
+
+
+def sphere(u, v):
+    """
+    Function that returns 3d coordinates for a cylinder from 2d input
+    Input: (u, v) -> (height[0-1], theta[0-1])
+    Ouput: (x, y, z) coordinate
+    """
+    radius = 2.0
+
+    v = math.radians(v * 360.0)
+    d = math.sqrt(radius ** 2 - u ** 2)
+
+    return(
+        radius * d * math.sin(v),
+        radius * d * math.cos(v),
+        u * radius
+    )
+
+
+def render_shape(z_res, xy_res, shape_func, multiplier = 1):
+    """
+    Function to render OpenGL shape given function of form T(u, v)
+    Input:  xy_res -> Resolutions of the xy for cylinder
+            z_res -> Resolutions of the z for cylinder
+    Output: renders OpenGl cylinder
+    """
+    # Get step size given subdivision
+    z_step = (1 / z_res)
+    xy_step = (1 / xy_res)
+
+    # Get inputs for cylinder function
+    z_res = round(z_res / 2) * multiplier
+    z_range = [x * z_step for x in range(-z_res, z_res)]
+    xy_range = [x * xy_step for x in range(0, xy_res)]
+
+    print(xy_range)
+
+    for u in z_range:
+        for v in xy_range:
+            glBegin(GL_QUAD_STRIP)
+
+            # Matrix for storing traingle strip vertex coordinates
+            # [1][2] - Top left | Top Right
+            # [0][3] - Bottom left | Bottom right
+            quad_mat = np.array([
+                shape_func(u, v),
+                shape_func(u, v + z_step),
+                shape_func(u + xy_step, v),
+                shape_func(u + xy_step, v + z_step),
+            ])
+
+            # Create side vertices
+            for i in range(4):
+                # p2 - p1
+                Vu = quad_mat[(i + 1) % 4] - quad_mat[i]
+                # p3 - p1                
+                Vv = quad_mat[(i + 2) % 4] - quad_mat[i]
+
+                # normal x = Uy * Vz - Uz * Vy
+                normal_x = Vu[1] * Vv[2] - Vu[2] * Vv[1]
+                # normal y = Uz * Vx - Ux * Vz
+                normal_y = Vu[2] * Vv[0] - Vu[0] * Vv[2]
+                # normal z = Ux * Vy - Uy * Vx
+                normal_z = Vu[0] * Vv[1] - Vu[1] * Vv[0]
+
+                # glTexCoord2d(0.2 * quad_mat[i][0], 0.2 * quad_mat[i][2])
+                glNormal3f(normal_x, normal_y, normal_z)
+                glTexCoord2d(quad_mat[i][0] / xy_res, quad_mat[i][2] / z_res)
+                glVertex3fv(quad_mat[i])
+
+            glEnd()
+
